095e25c16f2d3cc27e3bc129732c72139f828c40
Chikaki Ryotaro <r.chikaki@gmail.com>
2023-01-08 17:32:42 +0900

    <your_commit_message>

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
 # Uncomment this to pass the first stage
 import socket
+import threading
+
+
+def send_pong(client_socket):
+    while client_socket.recv(1024):
+        client_socket.send(b"+PONG\r\n")
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, client_address = server_socket.accept()  # wait for client
-
-    while client_socket.recv(1024):
-        client_socket.send(b"+PONG\r\n")
+    while True:
+        client_socket, _ = server_socket.accept()
+        thread = threading.Thread(target=send_pong, args=(client_socket,))
+        thread.start()
 
 
 if __name__ == "__main__":
