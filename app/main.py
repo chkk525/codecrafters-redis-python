7a0d9a6190fb223bba6c79a4cfe79aca9692333b
Chikaki Ryotaro <r.chikaki@gmail.com>
2023-01-08 18:16:15 +0900

    <your_commit_message>

--- app/main.py
+++ app/main.py
@@ -5,11 +5,18 @@ import threading
 
 def send_pong(client_socket):
     while True:
-        message = client_socket.recv(1024)
-        if message.lower() == 'pong':
-            client_socket.send(b"+PONG\r\n")
-        else:
-            print(message)
+        messages = client_socket.recv(1024).decode().split('\r\n')
+        # -> ['*2', '$4', 'echo', '$5', 'world', '']
+
+        for i, message in enumerate(messages):
+            if message.lower() == 'ping':
+                client_socket.send(b"+PONG\r\n")
+            if message.lower() == 'echo':
+                next_message = messages[i+2]
+                # Interpolate next message into a binary value to send.
+                client_socket.send(f"+{next_message}\r\n".encode())
+            else:
+                pass
 
 
 def main():
