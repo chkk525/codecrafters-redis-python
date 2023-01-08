c71454cea747df6ad8f3c99b83b2438f59cd5202
Chikaki Ryotaro <r.chikaki@gmail.com>
2023-01-08 21:39:52 +0900

    <your_commit_message>

--- app/main.py
+++ app/main.py
@@ -3,18 +3,24 @@ import socket
 import threading
 
 
-def send_pong(client_socket):
+def response(client_socket):
+    data = {}
     while True:
         messages = client_socket.recv(1024).decode().split('\r\n')
         # -> ['*2', '$4', 'echo', '$5', 'world', '']
 
         for i, message in enumerate(messages):
+            next_message = messages[i+2]
             if message.lower() == 'ping':
                 client_socket.send(b"+PONG\r\n")
             if message.lower() == 'echo':
                 next_message = messages[i+2]
                 # Interpolate next message into a binary value to send.
                 client_socket.send(f"+{next_message}\r\n".encode())
+            if message.lower() == 'set':
+                pass
+            if message.lower() == 'get':
+                pass
             else:
                 pass
 
@@ -23,7 +29,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
         client_socket, _ = server_socket.accept()
-        thread = threading.Thread(target=send_pong, args=(client_socket,))
+        thread = threading.Thread(target=response, args=(client_socket,))
         thread.start()
 
 
